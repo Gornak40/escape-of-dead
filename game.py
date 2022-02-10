daed93d4975c7aa2324e71e2776e7eae60112d6d
Gornak40 <s-kozelsk@yandex.ru>
2022-02-10 16:11:26 +0300

    better architecture (add methods)

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from bisect import bisect_left
+from random import randint
 
 
 MAX_LAWN = 6
@@ -6,9 +7,40 @@ MAX_BARRICADE = 10
 MAX_GARAGE = 10
 MAX_KILLS = 10
 GARAGE_LEVELS = [-1, 3, 6, 8, 10]
+LAWN_SUCCESS = 3
+BARRICADE_SUCCESS = 3
+GARAGE_SUCCESS = 5
 
 
 class Game:
+	def roll_dice(self):
+		return randint(1, 6)
+
+	def add_lawn(self, val):
+		self.lawn += val
+		self.lawn = min(self.lawn, MAX_LAWN)
+
+	def add_barricade(self, val):
+		self.barricade += val
+		self.barricade = min(self.barricade, MAX_BARRICADE)
+		if self.barricade <= 0:
+			self.lost()
+
+	def add_garage(self, val):
+		self.garage += val
+		if self.garage >= MAX_GARAGE:
+			self.win()
+
+	def add_kills(self, val):
+		self.kills += val
+		if self.kills >= MAX_KILLS:
+			self.kills -= MAX_KILLS
+			self.bonus()
+
+# TODO
+	def bonus(self):
+		print('Bonus')
+
 	def reset(self):	
 		self.lawn = 0
 		self.barricade = MAX_BARRICADE
@@ -23,35 +55,45 @@ class Game:
 
 	def win(self):
 		print('Victory!')
-		exit(0)
+		exit()
 
 	def lost(self):
 		print('You lost!')
-		exit(0)
+		exit()
 
-	def spawn(self):
-		add = bisect_left(GARAGE_LEVELS, self.garage)
-		add = min(MAX_LAWN, self.lawn + add) - self.lawn
-		self.lawn += add
-
-	def roll(self):
+	def roll_lawn(self):
 		pass
 
-	def damage(self):
-		self.barricade -= self.lawn
-		if self.barricade <= 0:
-			self.lost()
+	def roll_barricade(self):
+		pass
 
-	def set(self, *args):
-		match args[0]:
+	def roll_garage(self):
+		res = self.roll_dice()
+		print(f'Garage dice: {res}')
+		if res >= GARAGE_SUCCESS:
+			self.add_garage(1)
+
+	def roll(self):
+		line = list(input('Distribute dices: ').upper())
+		for i in range(line.count('L')):
+			self.roll_lawn()
+		for i in range(line.count('B')):
+			self.roll_barricade()
+		for i in range(line.count('G')):
+			self.roll_garage()
+
+	def add(self, *args):
+		tp, dlt = args
+		dlt = int(dlt)
+		match tp:
 			case 'lawn':
-				self.lawn = int(args[1])
+				self.add_lawn(dlt)
 			case 'barricade':
-				self.barricade = int(args[1])
+				self.add_barricade(dlt)
 			case 'garage':
-				self.garage = int(args[1])
+				self.add_garage(dlt)
 			case 'kills':
-				self.kills = int(args[1])
+				self.add_kills(dlt)
 
 	def __init__(self):
 		self.reset()
@@ -59,14 +101,14 @@ class Game:
 			command = [x.strip().lower() for x in input().split() if x.strip()]
 			match command[0]:
 				case 'exit':
-					exit(0)
+					exit()
 				case 'reset':
 					self.reset()
 				case 'info':
 					self.info()
 				case 'go':
-					self.spawn()
+					self.add_lawn(bisect_left(GARAGE_LEVELS, self.garage))
 					self.roll()
-					self.damage()
-				case 'set':
-					self.set(*command[1:])
+					self.add_barricade(-self.lawn)
+				case 'add':
+					self.add(*command[1:])
